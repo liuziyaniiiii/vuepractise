d8c74f4c105ee304d583cbcc3e373de8b31a942f
liu ziyan <212448600@qq.com>
2020-05-05 12:32:32 +0800

    oneday

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import TypeNav from '@/components/TypeNav'
 
 Vue.config.productionTip = false
 
+Vue.component('TypeNav',TypeNav)
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <TypeNav/>
+    <ListContainer/>
+    <TodayRecommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
+  </div>
+</template>
+
+<script>
+import ListContainer from './ListContainer/ListContainer'
+import TodayRecommend from './TodayRecommend/TodayRecommend'
+import Rank from './Rank/Rank'
+import Like from './Like/Like'
+import Floor from './Floor/Floor'
+import Brand from './Brand/Brand'
+
+export default {
+  name: 'Home',
+
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
